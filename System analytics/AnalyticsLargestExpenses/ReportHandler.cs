b3241e418c037f64460c7a62d9b294a2b7b454a4
Marina Mel <melm2004@mail.ru>
2024-04-25 12:48:40 +0300

    fix ReportHandler

--- System analytics/AnalyticsLargestExpenses/ReportHandler.cs
+++ System analytics/AnalyticsLargestExpenses/ReportHandler.cs
@@ -20,12 +20,17 @@ public class ReportHandler : IReportHandler
         Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
 
         var purchasesSorted = purchases
-            .OrderBy(p => p.OrderedAt);
+            .OrderBy(p => p.OrderedAt.Month);
 
         Dictionary<string, double> totalByMonth = [];
 
         foreach (var purchase in purchasesSorted)
         {
+            if (purchase.Status != CORRECT_STATUS)
+            {
+                continue;
+            }
+
             var month = purchase.OrderedAt
                 .ToString("MMMM")
                 .ToLower();
