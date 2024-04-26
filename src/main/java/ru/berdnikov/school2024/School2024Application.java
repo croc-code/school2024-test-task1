b89ab16f42c485a109040647a4d947bdf95a3d60
DBerdnikovO <dberdnikov0@gmail.com>
2024-04-26 18:59:08 +0400

    Complete

---
+++ src/main/java/ru/berdnikov/school2024/School2024Application.java
@@ -0,0 +1,11 @@
+package ru.berdnikov.school2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School2024Application {
+    public static void main(String[] args) {
+        SpringApplication.run(School2024Application.class, args);
+    }
+}
