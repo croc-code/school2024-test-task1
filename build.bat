2128ea77cf27f9def8bc3dbcdce98deaa72008a1
Marina Lebkova <m.lebkova@gmail.com>
2024-04-28 14:58:06 +0300

    add: build scripts

--- build.bat
+++
@@ -1,7 +0,0 @@
-conan install Conanfile.txt --build=missing
-cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE="./build/generators/conan_toolchain.cmake"
-cmake --build . --config Release
-cmake --install .
-cd ..\solution
-.\test_app.exe
