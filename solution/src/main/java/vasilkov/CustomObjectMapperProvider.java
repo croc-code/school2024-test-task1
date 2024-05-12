3a2145480f537f51bc47504e8f53f61c13c75f4b
Aleksandr <vasilkov.a.s@yandex.ru>
2024-05-12 03:19:06 +0300

    Update CustomObjectMapperProvider.java

--- solution/src/main/java/vasilkov/CustomObjectMapperProvider.java
+++ solution/src/main/java/vasilkov/CustomObjectMapperProvider.java
@@ -23,86 +23,15 @@ public class CustomObjectMapperProvider implements ObjectMapperProvider {
      */
     @Override
     public ObjectMapper getObjectMapper() {
+        
         ObjectMapper mapper = new ObjectMapper();
 
         // Регистрация модуля для работы с датами и временем в формате Java Time
         mapper.registerModule(new JavaTimeModule());
 
-        // Создание модуля для настройки сериализатора строк
-//        SimpleModule module = new SimpleModule();
-        // Добавление сериализатора для строк
-//        module.addSerializer(OrderModels.Result.class, new CustomStringSerializer2());
-
-        // Регистрация модуля в ObjectMapper
-//        mapper.registerModule(module);
-        // Отключение автоматического заключения имен полей в кавычки при сериализации JSON
         mapper.disable(JsonWriteFeature.QUOTE_FIELD_NAMES.mappedFeature());
 
         return mapper;
     }
 
-    /**
-     * Внутренний класс для кастомной сериализации строк.
-     */
-//    private static class CustomStringSerializer extends StdSerializer<String> {
-//
-//        protected CustomStringSerializer() {
-//            super(String.class);
-//        }
-//
-//        /**
-//         * Метод для сериализации строки с добавлением специальных символов.
-//         * @param value Строка для сериализации.
-//         * @param jsonGenerator Генератор JSON.
-//         * @param serializerProvider Провайдер сериализатора.
-//         * @throws IOException Исключение ввода-вывода.
-//         */
-//        @Override
-//        public void serialize(String value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-//            jsonGenerator.writeRawValue("«" + value + "»");
-//        }
-//    }
-//    private static class CustomStringSerializer extends StdSerializer<OrderModels.Result> {
-//        public CustomStringSerializer() {
-//            this(null);
-//        }
-//        public CustomStringSerializer(Class<OrderModels.Result> n) {
-//            super(n);
-//        }
-//        private String resultToSting(List<String> values) {
-//            StringBuilder resultSting = new StringBuilder("{«months»: [");
-//
-//            for (int i = 0; i < values.size(); ++i) {
-//                resultSting.append(values.get(i));
-//                if (i != values.size() - 1) {
-//                    resultSting.append(",");
-//                }
-//            }
-//
-//            return String.valueOf(resultSting);
-//        }
-//        @Override
-//        public void serialize(OrderModels.Result result, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-//            jsonGenerator.writeStartObject();
-//            jsonGenerator.writeFieldName("«months»");
-//
-//            jsonGenerator.writeRaw(": [");
-//            for (
-//                    Iterator<String> iterator = result.months().iterator();
-//                    iterator.hasNext();
-//                 )
-//            {
-//                String month = iterator.next();
-//                jsonGenerator.writeRaw("«" + month + "»");
-//                if (iterator.hasNext()) {
-//                    jsonGenerator.writeRaw(", ");
-//                }
-//            }
-//            jsonGenerator.writeRaw("]");
-//            jsonGenerator.writeEndObject();
-//
-//            jsonGenerator.writeString(resultToSting(result.months()));
-//
-//        }
-//    }
 }
